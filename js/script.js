e384322f418b9103b72c79f9cb2c9433cd2ea0d8
Yago Mendes <ygmendes21@gmail.com>
2023-06-13 19:31:29 -0300

    Atualização

--- js/script.js
+++ js/script.js
@@ -1,32 +1,64 @@
+const COLLISION_DELAY = 1000;
 const sonic = document.querySelector('.sonic');
 const pipe = document.querySelector('.pipe');
+let isJumping = false;
+let gameStopped = false;
 
 const jump = () => {
-  sonic.classList.add('sjump');
+  if (!isJumping && !gameStopped) {
+    isJumping = true;
+    sonic.classList.add('sjump');
+    sonic.src = 'image/pulinho.gif';
 
-  setTimeout(() => {
-    sonic.classList.remove('sjump');
-  }, 750);
+    setTimeout(() => {
+      sonic.classList.remove('sjump');
+      sonic.src = 'image/sonic.gif';
+      isJumping = false;
+    }, 750);
+  }
 };
 
-const loop = setInterval(() => {
-  console.log('loop');
+const stopGame = () => {
+  gameStopped = true;
+  clearInterval();
 
+  setTimeout(() => {
+    gameStopped = false;
+    startGame();
+  }, COLLISION_DELAY);
+};
+
+const restartGame = () => {
+  location.reload();
+};
+
+const checkCollision = () => {
   const pipePosition = pipe.offsetLeft;
   const sonicPosition = parseInt(window.getComputedStyle(sonic).bottom, 10);
+  const pipeOffset = 127;
+  const sonicBottom = sonicPosition < 60;
+  const isPipeVisible = pipePosition > 0;
 
-  if (pipePosition <= 115 && pipePosition > 0 && sonicPosition < 60) {
+  if (isPipeVisible && pipePosition <= pipeOffset && sonicBottom) {
     pipe.style.animation = 'none';
     pipe.style.left = `${pipePosition}px`;
-
-    pipe.style.animation = 'none';
     pipe.style.bottom = `${sonicPosition}px`;
 
     sonic.src = 'image/gameover.png';
     sonic.style.width = '150px';
 
-    clearInterval(loop);
+    stopGame();
+    setTimeout(restartGame, COLLISION_DELAY);
   }
-}, 10);
+};
+
+const startGame = () => {
+  loop = setInterval(() => {
+    console.log('loop');
+    checkCollision();
+  }, 10);
+};
 
 document.addEventListener('keydown', jump);
+
+startGame();

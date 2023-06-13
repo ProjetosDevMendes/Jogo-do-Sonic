20272609c29ca5a7378f2c6c0715e6090c0adda7
Yago Mendes <ygmendes21@gmail.com>
2023-06-13 13:46:57 -0300

    Atualização de Codigo

--- js/script.js
+++ js/script.js
@@ -2,39 +2,31 @@ const sonic = document.querySelector('.sonic');
 const pipe = document.querySelector('.pipe');
 
 const jump = () => {
-sonic.classList.add('sjump');
+  sonic.classList.add('sjump');
 
-setTimeout(() => {
+  setTimeout(() => {
     sonic.classList.remove('sjump');
-}, 1000);
-}
+  }, 750);
+};
 
 const loop = setInterval(() => {
+  console.log('loop');
 
-console.log('loop')
+  const pipePosition = pipe.offsetLeft;
+  const sonicPosition = parseInt(window.getComputedStyle(sonic).bottom, 10);
 
-    const pipePosition = pipe.offsetLeft;
-    const sonicPosition = +window.getComputedStyle(sonic).bottom.replace
-    ('px', '');
+  if (pipePosition <= 115 && pipePosition > 0 && sonicPosition < 60) {
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
 
+    pipe.style.animation = 'none';
+    pipe.style.bottom = `${sonicPosition}px`;
 
-    if (pipePosition <=  115 && pipePosition > 0 && sonicPosition < 60)
-    
-    {
+    sonic.src = 'image/gameover.png';
+    sonic.style.width = '150px';
 
-pipe.style.animation = 'none';
-pipe.style.left = `${pipePosition}px`;
-
-pipe.style.animation = 'none';
-pipe.style.bottom = `${sonicPosition}px`;
-
-sonic.src = 'image/gameover.png'
-sonic.style.width = '150px'
-
-clearInterval(loop)
-
-    }
-
+    clearInterval(loop);
+  }
 }, 10);
 
 document.addEventListener('keydown', jump);
